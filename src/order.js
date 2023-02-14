f2230bff131e13e8175c2702ea6540ef0a40a8d1
Chloe <31783292+chloeqino@users.noreply.github.com>
2023-02-13 21:01:14 -0800

    update ui

--- src/order.js
+++ src/order.js
@@ -1,167 +1,199 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router,Routes, Route, Link,useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
 import Server from "./server";
-import Modal from 'react-modal';
-import MenuOrOrder from './components/MenuOrCart';
-export default function Order(){
-
-    
-   let id = useParams().itemid;
-   const [itemid,setId] = useState(id);
-   const [qty,setQty] = useState(1);
-   const [modalIsOpen, setIsOpen] = React.useState(false);
-   //Modal.setAppElement("#main");
-   const [toppings_checked,setChecked] = useState(Array(new Server().Toppings.length).fill(false));
-   const [sugar,setSugar] = useState(100);
-   const [price,setPrice] = useState(updatePrice());
-   let c = toppings_checked;
-   let cartnum = null;
-   function openModal() {
+import Modal from "react-modal";
+import MenuOrOrder from "./components/MenuOrCart";
+export default function Order() {
+  let id = useParams().itemid;
+  const [itemid, setId] = useState(id);
+  const [qty, setQty] = useState(1);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  //Modal.setAppElement("#main");
+  const [toppings_checked, setChecked] = useState(
+    Array(new Server().Toppings.length).fill(false)
+  );
+  const [sugar, setSugar] = useState(100);
+  const [price, setPrice] = useState(updatePrice());
+  let c = toppings_checked;
+  let cartnum = null;
+  function openModal() {
     setIsOpen(true);
   }
-
-  
 
   function closeModal() {
     setIsOpen(false);
   }
-   //setsetChecked(c);
-   //console.log(toppings_checked);
-  
-    let iteminfo = new Server().Items[itemid]; 
-    function toggleSugar(e){
-        setSugar(e.target.value);
-        console.log(e.target.value);
-    }
-    function updatePrice(){
-        return (new Server().Items[id].price+0.5*toppings_checked.filter(Boolean).length)*qty;
-    }  
-    function renderSugarLevels(){
-    return new Server().sugar_levels.map(
-        (e,i)=>{
-            return (<div className='sugarLevel'>
-                <input type="radio" name="sugar" value={e.value} id={e.id} key = {e.id}
-                checked = {e.value==sugar} onChange = {toggleSugar} /><label htmlFor={e.id}>{e.value+'%'}</label>
-            </div>);
-        }
-    );
-    }
-    function renderToppings(){
-        return new Server().Toppings.map(
-            (e,i)=>{
-                //console.log(i);
-                return (<div className='topping'><input type = "checkbox" key = {e.id} id={e.id} name={e.id} value={e.name} className='toppingoption'
-                defaultChecked = {toppings_checked[i]} onChange = {event=>{
-                    let checks = toppings_checked;
-                    
-                    checks[i] = !checks[i];
-                    //console.log(checks);
-                    setChecked(checks);
-                    //console.log(toppings_checked);
-                    setPrice(updatePrice());
-                }} /><label htmlFor={e.id}>{e.name}</label></div>);
-            }
-        );
-    }
-    function handleSubmit(e){
-        e.preventDefault();
-        
-        console.log(Math.max(1,qty)+1);
-        console.log("toppings "+toppings_checked);
-        console.log("sugar "+sugar);
-        let o = {};
-        o["itemid"] = itemid;
-        o["sugar"] =sugar;
-        o["toppings"] = new Server().Toppings.filter((e,i)=>{
-            return toppings_checked[i];
-        });
-        for(let i = 0; i<Math.max(1,qty)+1-1;i++ ){
-            new Server().addtoCart(o);
-        }
-        console.log(JSON.stringify(o));
-        document.getElementById("cartnum").textContent = new Server().CartItems.length;
-        openModal();
+  //setsetChecked(c);
+  //console.log(toppings_checked);
 
+  let iteminfo = new Server().Items[itemid];
+  function toggleSugar(e) {
+    setSugar(e.target.value);
+    console.log(e.target.value);
+  }
+  function updatePrice() {
+    return (
+      (new Server().Items[id].price +
+        0.5 * toppings_checked.filter(Boolean).length) *
+      qty
+    );
+  }
+  function renderSugarLevels() {
+    return new Server().sugar_levels.map((e, i) => {
+      return (
+        <div className="sugarLevel">
+          <input
+            type="radio"
+            name="sugar"
+            value={e.value}
+            id={e.id}
+            key={e.id}
+            checked={e.value == sugar}
+            onChange={toggleSugar}
+          />
+          <label htmlFor={e.id}>{e.value + "%"}</label>
+        </div>
+      );
+    });
+  }
+  function renderToppings() {
+    return new Server().Toppings.map((e, i) => {
+      //console.log(i);
+      return (
+        <div className="topping">
+          <input
+            type="checkbox"
+            key={e.id}
+            id={e.id}
+            name={e.id}
+            value={e.name}
+            className="toppingoption"
+            defaultChecked={toppings_checked[i]}
+            onChange={(event) => {
+              let checks = toppings_checked;
+
+              checks[i] = !checks[i];
+              //console.log(checks);
+              setChecked(checks);
+              //console.log(toppings_checked);
+              setPrice(updatePrice());
+            }}
+          />
+          <label htmlFor={e.id}>{e.name}</label>
+        </div>
+      );
+    });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(Math.max(1, qty) + 1);
+    console.log("toppings " + toppings_checked);
+    console.log("sugar " + sugar);
+    let o = {};
+    o["itemid"] = itemid;
+    o["sugar"] = sugar;
+    o["toppings"] = new Server().Toppings.filter((e, i) => {
+      return toppings_checked[i];
+    });
+    for (let i = 0; i < Math.max(1, qty) + 1 - 1; i++) {
+      new Server().addtoCart(o);
     }
-     return (<main id='orderpage'>
-         <Modal
+    console.log(JSON.stringify(o));
+    document.getElementById("cartnum").textContent =
+      new Server().CartItems.length;
+    openModal();
+  }
+  return (
+    <main id="orderpage">
+      <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         contentLabel="Example Modal"
       >
-          
-          <h4>Added to Cart!</h4>
-              <MenuOrOrder />
-          <button onClick={closeModal} className='closeBtn'>X</button>
-          
-          </Modal>
-         <div className='wrapper'>
-            
-         
-      
-             <nav><Link to="/e-commerce/menu">menu</Link>/{iteminfo.title}</nav>
-            
-             
-             <div id="orderform" className='row'>
-             <div className='col' id='itemImgContainer'>
-             <img src={iteminfo.imageUrl} alt={iteminfo.title} id='itemImg'></img>
-             </div>
-             <div id='form'>
-                 
-         
-     
-     <form onSubmit={handleSubmit} id='form1'>
-      <fieldset> 
-          
-     <legend>{iteminfo.title}</legend>
-     <input type="hidden" name="itemId" value={itemid} />
-     
-    
-         <p>
-    <label htmlFor="qty">quantity: </label><input type="number" min="1" step="1" value={qty} id="qty" onChange = {e=>{
-       let v =e.target.value;
-       if(v<1&v!=null&v!=''){
-           v = 1;
-       }
-      if(v==''){
-          setQty('');
-          
-          return;
-          console.log("empty");
-      }
-         
-        setQty(Number(v));
-        
-        console.log(price);
-        
-    }} onKeyPress={(event) => {
-        if (!/[0-9]/.test(event.key)) {
-          event.preventDefault();
-        }
-      }} />
-      </p>
-      <p>
-          Sugar level: <br/>
-          {renderSugarLevels()}
-      </p>
-      <p> select toppings: (Optional)<br />
-         {renderToppings()}</p>
-         <input type="submit" value="Add to Cart" form='form1'/>
-         </fieldset> 
-         
-     
-     
-     
-     </form>
-    
-     </div>
-   
-     </div>
-     
-     </div>
-     </main>);
-    
-}
+        <h4>Added to Cart!</h4>
+        <MenuOrOrder />
+        <button onClick={closeModal} className="closeBtn">
+          X
+        </button>
+      </Modal>
+      <div className="wrapper">
+        <nav>
+          <Link to="/e-commerce/menu">menu</Link>/{iteminfo.title}
+        </nav>
 
+        <div id="orderform" className="row">
+          <div className="col" id="itemImgContainer">
+            <img
+              src={iteminfo.imageUrl}
+              alt={iteminfo.title}
+              id="itemImg"
+            ></img>
+          </div>
+          <div id="form">
+            <form onSubmit={handleSubmit} id="form1">
+              <fieldset>
+                <legend>{iteminfo.title}</legend>
+                <input type="hidden" name="itemId" value={itemid} />
+
+                <p>
+                  <label htmlFor="qty" className="subtitle">
+                    quantity:{" "}
+                  </label>
+                  <input
+                    type="number"
+                    min="1"
+                    step="1"
+                    value={qty}
+                    id="qty"
+                    onChange={(e) => {
+                      let v = e.target.value;
+                      if ((v < 1) & (v != null) & (v != "")) {
+                        v = 1;
+                      }
+                      if (v == "") {
+                        setQty("");
+
+                        return;
+                        console.log("empty");
+                      }
+
+                      setQty(Number(v));
+
+                      console.log(price);
+                    }}
+                    onKeyPress={(event) => {
+                      if (!/[0-9]/.test(event.key)) {
+                        event.preventDefault();
+                      }
+                    }}
+                  />
+                </p>
+                <p>
+                  <label className="subtitle">Sugar level: </label>
+                  {renderSugarLevels()}
+                </p>
+                <p>
+                  {" "}
+                  <label className="subtitle">
+                    select toppings: (Optional)
+                  </label>
+                  {renderToppings()}
+                </p>
+                <input type="submit" value="Add to Cart" form="form1" />
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/server.js
+++ src/server.js
@@ -1,170 +1,169 @@
-class Server{
-    myStorage = window.localStorage;
-   
-    items = {
-        "item00":{
-            "id":"item00",
-            "title":"Milk Tea",
-            "price":3.0,
-            "imageUrl":require('./assets/bobapink.png')
-        },
-        "item01":{
-            "id":"item01",
-            "title":"Thai Tea",
-            "price":3.5,
-            "imageUrl":require('./assets/thai.png')
-        },
-        "item02":{
-            "id":"item02",
-            "title":"Green Tea",
-            "price":2,
-            "imageUrl":require('./assets/green.png')
-        },
-        "item03":{
-            "id":"item03",
-            "title":"Coconut Milk Tea",
-            "price":4,
-            "imageUrl":require('./assets/coconut.png')
-        },
-        "item04":{
-            "id":"item04",
-            "title":"Wicked Bubble Tea",
-            "price":4.5,
-            "imageUrl":require('./assets/magic.png')
-        },
-        "item05":{
-            "id":"item05",
-            "title":"Rose Bubble Tea",
-            "price":4,
-            "imageUrl":require('./assets/rose.png')
-        }
-    };
-    toppings = [
-        {
-            "name":"grass jelly",
-            "id":"t0"
-        },
-        {
-            "name":"pearl",
-            "id":'t1'
-        },
-        {
-            "name":"sago",
-            "id":"t2"
-        }
+class Server {
+  myStorage = window.localStorage;
 
-    ];
-    sugar_levels = [
-     {
-         "value": 100,
-         "id":"sugar100"
-     },
-     {
-         "value":70,
-         "id":"sugar70"
-     },
-     {
-        "value":50,
-        "id":"sugar50"
+  items = {
+    item00: {
+      id: "item00",
+      title: "Milk Tea",
+      price: 3.0,
+      imageUrl: require("./assets/bobapink.png"),
+    },
+    item01: {
+      id: "item01",
+      title: "Thai Tea",
+      price: 3.5,
+      imageUrl: require("./assets/thai.png"),
+    },
+    item02: {
+      id: "item02",
+      title: "Green Tea",
+      price: 2,
+      imageUrl: require("./assets/green.png"),
+    },
+    item03: {
+      id: "item03",
+      title: "Coconut Milk Tea",
+      price: 4,
+      imageUrl: require("./assets/coconut.png"),
+    },
+    item04: {
+      id: "item04",
+      title: "Wicked Bubble Tea",
+      price: 4.5,
+      imageUrl: require("./assets/magic.png"),
+    },
+    item05: {
+      id: "item05",
+      title: "Rose Bubble Tea",
+      price: 4,
+      imageUrl: require("./assets/rose.png"),
+    },
+  };
+  toppings = [
+    {
+      name: "grass jelly",
+      id: "t0",
     },
     {
-        "value":30,
-        "id":"sugar30"
+      name: "pearl",
+      id: "t1",
     },
     {
-        "value":0,
-        "id":"sugar0"
+      name: "sago",
+      id: "t2",
     },
-    ];
-     logStorage(){
-        console.log(this.myStorage);
-    }
-    get Toppings(){
-        return this.toppings;
-    }
-    get Items(){
-        return this.items;
-    }
-    EditItem(orderid, sugar,toppings){
-      let index = -1;
-      let newcart = JSON.parse(this.myStorage.getItem('cart'));
-      let oldcart = JSON.parse(this.myStorage.getItem('cart'));
-      for(let i = 0;i<oldcart.length;i++){
-          if(oldcart[i].id==orderid){
-              newcart[i].toppings = toppings;
-              newcart[i].sugar = String(sugar);
-              break;
-          }
+    {
+      name: "lychee jelly",
+      id: "t3",
+    },
+  ];
+  sugar_levels = [
+    {
+      value: 100,
+      id: "sugar100",
+    },
+    {
+      value: 70,
+      id: "sugar70",
+    },
+    {
+      value: 50,
+      id: "sugar50",
+    },
+    {
+      value: 30,
+      id: "sugar30",
+    },
+    {
+      value: 0,
+      id: "sugar0",
+    },
+  ];
+  logStorage() {
+    console.log(this.myStorage);
+  }
+  get Toppings() {
+    return this.toppings;
+  }
+  get Items() {
+    return this.items;
+  }
+  EditItem(orderid, sugar, toppings) {
+    let index = -1;
+    let newcart = JSON.parse(this.myStorage.getItem("cart"));
+    let oldcart = JSON.parse(this.myStorage.getItem("cart"));
+    for (let i = 0; i < oldcart.length; i++) {
+      if (oldcart[i].id == orderid) {
+        newcart[i].toppings = toppings;
+        newcart[i].sugar = String(sugar);
+        break;
       }
-      this.myStorage.setItem("cart",JSON.stringify(newcart));
-      
     }
-    emptyCart(){
-        this.myStorage.setItem("cart","[]");
-        this.myStorage.setItem("orderNumber","0");
+    this.myStorage.setItem("cart", JSON.stringify(newcart));
+  }
+  emptyCart() {
+    this.myStorage.setItem("cart", "[]");
+    this.myStorage.setItem("orderNumber", "0");
+  }
+  deleteCartItem(index) {
+    let items = this.myStorage.getItem("cart");
+    items = JSON.parse(items);
+    items = items.filter((e, i) => {
+      return e.id != index;
+    });
+
+    this.myStorage.setItem("cart", JSON.stringify(items));
+  }
+  duplicateCartItem(orderId) {
+    let items = this.myStorage.getItem("cart");
+    items = JSON.parse(items);
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].id == orderId) {
+        let o = JSON.parse(JSON.stringify(items[i]));
+        o["id"] = this.OrderId;
+
+        items.splice(i, 0, o);
+        console.log(items);
+        this.myStorage.setItem("cart", JSON.stringify(items));
+        break;
+      }
     }
-    deleteCartItem(index){
-        let items = this.myStorage.getItem("cart");
-        items = JSON.parse(items);
-        items = items.filter((e,i)=>{
-            return e.id!=index;
-        });
-        
-        this.myStorage.setItem("cart",JSON.stringify(items));
-    }
-    duplicateCartItem(orderId){
-        let items = this.myStorage.getItem("cart");
-        items = JSON.parse(items);
-        for (let i =0; i<items.length;i++){
-            if(items[i].id==orderId){
-                let o = JSON.parse(JSON.stringify(items[i]));
-                o["id"] =  this.OrderId;
-                
-                items.splice(i,0,o);
-                console.log(items);
-                this.myStorage.setItem("cart",JSON.stringify(items));
-                break;
-            }
-        }
-    }
-    get CartItems(){
-        if(this.myStorage.getItem("cart")){
-        let items = JSON.parse(this.myStorage.getItem("cart"));
-        for(let i = 0; i< items.length;i++)
-        {
-            //console.log(items[i]);
+  }
+  get CartItems() {
+    if (this.myStorage.getItem("cart")) {
+      let items = JSON.parse(this.myStorage.getItem("cart"));
+      for (let i = 0; i < items.length; i++) {
+        //console.log(items[i]);
         //items[i]["toppings"] = JSON.parse(items[i]["toppings"]);
         items[i]["sugar"] = Number(items[i]["sugar"]);
-        }
-        return items;
-        }
-       return [];
+      }
+      return items;
     }
-    get OrderId(){
-        let n = 0;
-        if(!this.myStorage.getItem("orderNumber"))
-        {
-             this.myStorage.setItem("orderNumber","0");
-        }
-        n = Number(this.myStorage.getItem("orderNumber"))+1;
-        this.myStorage.setItem("orderNumber",n.toString());
-        return "order"+n;
+    return [];
+  }
+  get OrderId() {
+    let n = 0;
+    if (!this.myStorage.getItem("orderNumber")) {
+      this.myStorage.setItem("orderNumber", "0");
     }
-    //receive an object denoting an order and add it to the local storage
-    addtoCart(orderitem){
-        if(!this.myStorage.getItem('cart')){
-            this.myStorage.setItem("cart","[]");
-        }
-        let o = orderitem;
-        o["id"] = this.OrderId;
-        console.log(o);
-        console.log(this.myStorage.getItem('cart'));
-        let currentorders = JSON.parse(this.myStorage.getItem('cart'));
-        
-        currentorders.unshift(o);
-       this.myStorage.setItem("cart",JSON.stringify(currentorders));
+    n = Number(this.myStorage.getItem("orderNumber")) + 1;
+    this.myStorage.setItem("orderNumber", n.toString());
+    return "order" + n;
+  }
+  //receive an object denoting an order and add it to the local storage
+  addtoCart(orderitem) {
+    if (!this.myStorage.getItem("cart")) {
+      this.myStorage.setItem("cart", "[]");
     }
-}   
+    let o = orderitem;
+    o["id"] = this.OrderId;
+    console.log(o);
+    console.log(this.myStorage.getItem("cart"));
+    let currentorders = JSON.parse(this.myStorage.getItem("cart"));
 
-export default Server; 
+    currentorders.unshift(o);
+    this.myStorage.setItem("cart", JSON.stringify(currentorders));
+  }
+}
 
+export default Server;
